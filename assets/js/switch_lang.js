a37240660bc31d823833c0fe6e3e21e00ded3fcd
Nozomi Maki <40193664+makinzm@users.noreply.github.com>
2023-04-02 17:51:07 +0900

    update to apply to English (#3)

---
+++ assets/js/switch_lang.js
@@ -0,0 +1,22 @@
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+function setLanguageElements(language) {
+  const enOnlyElements = document.querySelectorAll('.en-only');
+  const jaOnlyElements = document.querySelectorAll('.ja-only');
+
+  if (language === 'ja') {
+    enOnlyElements.forEach(element => element.style.display = 'none');
+    jaOnlyElements.forEach(element => element.style.display = 'block');
+  } else {
+    enOnlyElements.forEach(element => element.style.display = 'block');
+    jaOnlyElements.forEach(element => element.style.display = 'none');
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lang = getQueryParam('lang') || 'en';
+  setLanguageElements(lang);
+})
